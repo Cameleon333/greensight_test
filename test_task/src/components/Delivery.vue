e60eb129cea93989f82bfd0d475ad9e570a22b6c
Cmleon <igrook33@gmail.com>
2019-10-23 00:55:19 +0300

    delivery page

---
+++ test_task/src/components/Delivery.vue
@@ -0,0 +1,123 @@
+<template>
+<div class="main">
+    <form action="" name="delivery-form">
+        <div class="group" id="container-fio">
+            <input type="text" id="fio" placeholder="Только кириллица" :pattern="maskFio">
+            <label for="fio">ФИО</label>
+        </div>
+        <div class="group">
+            <input type="tel" id="phone" placeholder="+7 (___) ___-__-__" :pattern="maskTel">
+            <label for="phone">Телефон</label>
+        </div>
+        <div class="group wide">
+            <input type="text" id="delivery-address" placeholder="Город, улица, дом">
+            <label for="delivery-address">Адрес доставки</label>
+        </div>
+        <div class="group wide">
+            <textarea name="comment" id="comment" class="comment"></textarea>
+            <label for="comment" id="comment-label">Комментарий</label>
+        </div>
+        <div class="group wide button">
+            <input type="submit" text="Отправить">
+        </div>
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Delivery',
+    data() {
+        return {
+            maskTel: "+7 ([0-9]{3}([0-9]{3})([0-9]{2})([0-9]{2})",
+            maskFio: "^[А-Яа-яЁё\\s]+$",
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    @border-color: #b8bed8;
+    @label-color: #3f3f3f;
+    @input-disabled-color: #edeeef;
+
+    .main {
+        font-family: 'Montserrat', sans-serif;
+        font-weight: 300;
+        font-size: 14px;
+    }
+
+    label {
+        color: @label-color;
+        position: relative;
+        bottom: 90px;
+    }
+    #comment-label {
+        bottom: 165px;
+    }
+    
+    input, .comment {
+        display: inline-block;
+        min-width: 220px;
+        width: 100%;
+        padding: 20px;
+        border-radius: 3px;
+        font-family: inherit;
+        font-size: inherit;
+        font-weight: inherit;
+        border-style: none;
+        border: 1px solid @border-color;
+        margin-top: 10px;
+        box-sizing: border-box;
+        outline: none;
+        cursor: pointer;
+    }
+    input::placeholder {
+        color: @border-color;
+    }
+    input[type="tel"]::placeholder {
+        letter-spacing: 2px;
+    }
+    input:hover, .comment:hover {
+        border-color: @border-color - #111111;
+    }
+    input:focus, .comment:focus {
+        border-color: @border-color - #222222;
+    }
+    input:focus::placeholder {
+        font-size: 12px;
+    }
+    input:disabled, .comment:disabled {
+        background-color: @input-disabled-color;
+        border-color: @input-disabled-color;
+        cursor: default;
+        ~label {
+            color: @border-color;
+        }
+    }
+
+    .group {
+        display: inline-block;
+        width: calc(50% - 20px);
+        margin-top: 30px;
+        white-space: pre-wrap;
+    }
+
+    #container-fio {
+        margin-right: 40px;
+    }
+
+    .wide {
+        width: 100%;
+    }
+
+    .comment {
+        resize: none;
+        height: 132px;
+    }
+
+    .button {
+        text-align: right;
+        margin-top: 0;
+    }
+</style>
--- test_task/src/components/TabsBar.vue
+++ test_task/src/components/TabsBar.vue
@@ -5,13 +5,19 @@
             <div class="tabs" :class="{ activeTab: tab.isActive}" v-for="(tab, index) in tabList" :key="index" @click="selectTab(index)"><span class="tab-text">{{ tab.text }}</span></div>
         </div>
         <div class="tab-content">
+            <delivery v-if="tabList[0].isActive"/>
         </div>
     </div>
 </template>
 
 <script>
+import Delivery from "./Delivery.vue"
+
 export default {
     name: 'TabsBar',
+    components: {
+        Delivery,
+    },
     data() {
         return {
             tabList: [
@@ -40,43 +46,48 @@ export default {
 </script>
 
 <style lang="less" scoped>
-    @font: sans-serif;
-    @selectedTextColor: #212bff;
+    @font: 'Montserrat', sans-serif;
+    @selected-text-color: #212bff;
+    @tabs-background-color: #edeeef;
+    @tabs-text-color: #8288a2;
+    @tabs-before-border-bottom-color: #dadbdc;
+    @tabs-after-border-bottom-color: #edeeef;
 
     #main {
         min-width: 320px;
         max-width: 1200px;
-        width: 100%;
-        margin: auto;
+        margin: 0 auto;
+        // display: inline-block;
+        vertical-align: top;
     }
 
     h2 {
         font-family: @font;
         font-size: 32px;
-        font-weight: 300;
+        font-weight: 400;
         line-height: 32px;
         margin-top: 40px;
-        letter-spacing: 2px;
+        margin-bottom: 40px;
     }
 
     .tabs {
         width: 140px;
         height: 70px;
-        background-color: #edeeef;
-        color: #8288a2;
+        background-color: @tabs-background-color;
+        color: @tabs-text-color;
         display: inline-block;
         border-radius: 5px 5px 0 0;
         position: relative;
         cursor: pointer;
     }
     .tabs:hover {
-        color: @selectedTextColor;
+        color: @selected-text-color;
     }
 
     .tab-text {
         display: block;
         font-family: @font;
-        font-size: 16px;
+        font-size: 14px;
         font-weight: 300;
         margin-left: 30px;
         margin-top: 28px;
@@ -88,7 +99,7 @@ export default {
         width: 0;
         height: 0;
         border-right: 30px solid transparent;
-        border-bottom: 68px solid #dadbdc;
+        border-bottom: 68px solid @tabs-before-border-bottom-color;
         position: absolute;
         bottom: 0;
         right: -29.5px;
@@ -100,7 +111,7 @@ export default {
         width: 0;
         height: 0;
         border-right: 30px solid transparent;
-        border-bottom: 68px solid #edeeef;
+        border-bottom: 68px solid @tabs-after-border-bottom-color;
         position: absolute;
         bottom: 0;
         right: -27.5px;
@@ -110,7 +121,7 @@ export default {
     // For selected tab
     .activeTab {
         background-color: white;
-        color: @selectedTextColor;
+        color: @selected-text-color;
         z-index: 21;
     }
     .activeTab::after {
@@ -118,8 +129,39 @@ export default {
     }
 
     .tab-content {
-        width: 100%;
+        min-width: 320px;
+        max-width: 1200px;
         background-color: white;
-        padding: 40px 30px;
+        padding: 20px 30px;
+        // display: inline-block;
+    }
+
+
+</style>
+
+<style lang="less">
+    // For submit button
+    @button-color: #212bff;
+    @button-color-disabled: #edeeef;
+    @button-disabled-text-color: #b8bed8;
+
+    input[type="submit"] {
+        height: 60px;
+        width: 80px;
+        border-radius: 30px;
+        background-color: @button-color;
+        color: white;
+        cursor: pointer;
+    }
+    input[type="submit"]:hover {
+        background-color: @button-color - #111111;
+    }
+    input[type="submit"]:active {
+        background-color: @button-color - #333333;
+    }
+    input[type="submit"]:disabled {
+        background-color: @button-color-disabled;
+        color: @button-disabled-text-color;
+        cursor: default;
     }
 </style>
